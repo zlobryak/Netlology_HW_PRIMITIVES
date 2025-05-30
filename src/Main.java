2c41d7f2b65ddb1fbaf8d9d51216c67e73a8eca7
Zlobryak <clychnik@gmail.com>
2025-05-30 21:29:04 +0300

    minor fix

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
 //Цикл будет работать, пока пользователь не введет `end`
         while (true) {
             // Выводим информацию о возможных операциях пользователю
+            System.out.println();
             System.out.println("Выберите операцию и введите её номер:");
             System.out.println("1. Добавить новый доход");
             System.out.println("2. Добавить новый расход");
             System.out.println("3. Выбрать систему налогообложения");
-            System.out.println("3 <Enter>");
 
             String input = scanner.nextLine();
             if ("end".equals(input)) {
@@ -41,18 +41,17 @@ public class Main {
                     // действия при выборе третьей операции
                     if(Calck.taxEarnings(earnings) < Calck.taxEarningsMinusSpendings(earnings, spendings)) {
                         System.out.println("Мы советуем вам УСН доходы");
-                        System.out.println("Ваш налог составит: " + Calck.taxEarnings(earnings) + "рублей");
-                        System.out.println("Налог на другой системе:" + Calck.taxEarningsMinusSpendings(earnings, spendings) + "рублей");
+                        System.out.println("Ваш налог составит: " + Calck.taxEarnings(earnings) + " рублей");
+                        System.out.println("Налог на другой системе:" + Calck.taxEarningsMinusSpendings(earnings, spendings) + " рублей");
                         System.out.println("Экономия: " + Calck.ecoCalck(earnings, spendings) + " рублей");
 
                     }else{
                         System.out.println("Мы советуем вам УСН доходы минус расходы");
                         System.out.println("Ваш налог составит: "+ " Calck.taxEarningsMinusSpendings(earnings, spendings)" + " рублей");
-                        System.out.println("Налог на другой системе:" + Calck.taxEarnings(earnings) + "рублей");
-                        System.out.println("Экономия: " + Calck.ecoCalck(earnings, spendings) + "рублей");
+                        System.out.println("Налог на другой системе:" + Calck.taxEarnings(earnings) + " рублей");
+                        System.out.println("Экономия: " + Calck.ecoCalck(earnings, spendings) + " рублей");
 
                     }
-
                     break;
                 default:
                     System.out.println("Такой операции нет");
